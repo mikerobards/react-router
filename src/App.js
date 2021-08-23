669c178657a1474ed4a85700318236ba56b68e2b
Michael Robards <mikerobards@icloud.com>
2021-08-23 16:45:49 -0400

    add Contact

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { Link, Switch, Route } from 'react-router-dom';
 
 import Home from './Home';
 import About from './About';
+import Contact from './Contact'
 
 function App() {
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
 
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
+        <Route exact path="/"><Home /></Route>
+        <Route path="/about"><About /></Route>
+        <Route path="/contact"><Contact /></Route>
       </Switch>
     </div>
   );
