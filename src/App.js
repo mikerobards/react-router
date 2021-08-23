1d0fd769cf23ee11704b5b0bb50d79d2c85dd095
Michael Robards <mikerobards@icloud.com>
2021-08-23 16:29:43 -0400

    previous way of using Switch and Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
   );
 }
